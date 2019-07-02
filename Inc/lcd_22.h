21a4b31eaf4b9a8fa41746d0e2f8114b4deed449
Victor Nikitchuk <rainbowkiwifox@gmail.com>
2019-07-03 02:42:20 +0300

    Переделал типы данных, перенёс шрифты, начал разбираться в функиях

--- Inc/lcd_22.h
+++ Inc/lcd_22.h
@@ -1,58 +1,81 @@
+/*
+*========================================================================================================
+*
+* File                : LCD_22.C
+* Hardware Environment: STM32F446
+* Build Environment   : Keil uVision 5
+* Version             : 1.0
+* By                  : Victor Nikitchuk & WaveShare Indian programmers
+*
+*	Основано на примере для Open16F877A. https://www.waveshare.com/wiki/File:Open16F877A-Demo.7z
+*
+*========================================================================================================
+*/
+
 #ifndef LCD_22_H
 #define LCD_22_H
 
 #include "main.h"
+/* Макросы взаимодействия с портами ввода/вывода */
+//Чтение логического уровня на пине прерывания от тачскрина
+#define PENIRQ						(HAL_GPIO_ReadPin(T_IRQ_GPIO_Port, T_IRQ_Pin) == GPIO_PIN_SET)
+//Опускание/поднятие логического уровня CS тачскрина
+#define Touch_CS_Reset		HAL_GPIO_WritePin(T_CS_GPIO_Port, T_CS_Pin, GPIO_PIN_RESET)
+#define Touch_CS_Set			HAL_GPIO_WritePin(T_CS_GPIO_Port, T_CS_Pin, GPIO_PIN_SET)
+//Опускание/поднятие логического уровня ножки сброса дисплея
+#define TFT_RESET_Reset		HAL_GPIO_WritePin(RESET_GPIO_Port, RESET_Pin, GPIO_PIN_RESET)  
+#define TFT_RESET_Set			HAL_GPIO_WritePin(RESET_GPIO_Port, RESET_Pin, GPIO_PIN_SET)
+//Опускание/поднятие логического уровня CS дисплея
+#define TFT_CS_Reset			HAL_GPIO_WritePin(D_CS_GPIO_Port, D_CS_Pin, GPIO_PIN_RESET)
+#define TFT_CS_Set				HAL_GPIO_WritePin(D_CS_GPIO_Port, D_CS_Pin, GPIO_PIN_SET)
+//Передача данных/команды в дисплей
+#define TFT_data					HAL_GPIO_WritePin(D_RS_GPIO_Port, D_RS_Pin, GPIO_PIN_SET)
+#define TFT_index					HAL_GPIO_WritePin(D_RS_GPIO_Port, D_RS_Pin, GPIO_PIN_RESET)
 
-#define u16 uint16_t
-#define u8 uint8_t 
-
-#define LCD22_SPI
-
-#define PENIRQ						(HAL_GPIO_ReadPin(T_IRQ_GPIO_Port, T_IRQ_Pin) == GPIO_PIN_SET) //RA0   //IN
-
-#define en_touch()				HAL_GPIO_WritePin(T_CS_GPIO_Port, T_CS_Pin, GPIO_PIN_RESET)	//{RA4=0;}// T_CS 
-#define dis_touch()				HAL_GPIO_WritePin(T_CS_GPIO_Port, T_CS_Pin, GPIO_PIN_SET)	//{RA4=1;}
-
-#define reset_clr					HAL_GPIO_WritePin(RESET_GPIO_Port, RESET_Pin, GPIO_PIN_RESET)	//{RD0=0;}//RST    
-#define reset_set					HAL_GPIO_WritePin(RESET_GPIO_Port, RESET_Pin, GPIO_PIN_SET)	//{RD0=1;}//
-
-#define en_lcd()					HAL_GPIO_WritePin(D_CS_GPIO_Port, D_CS_Pin, GPIO_PIN_RESET)	//{RA1=0;}// CS
-#define dis_lcd()					HAL_GPIO_WritePin(D_CS_GPIO_Port, D_CS_Pin, GPIO_PIN_SET)	//{RA1=1;}//
-
-#define en_lcd_data()			HAL_GPIO_WritePin(D_RS_GPIO_Port, D_RS_Pin, GPIO_PIN_SET)	//{RA2=1;}// RS
-#define en_lcd_index()		HAL_GPIO_WritePin(D_RS_GPIO_Port, D_RS_Pin, GPIO_PIN_RESET)	//{RA2=0;}//
-
+//TODO: Разобраться что за макросы
 #define SAMP_COUNT				5
 #define SAMP_THRESHOLD		5
 
 #define TOUCH_CMD_X 		0xD0
 #define TOUCH_CMD_Y 		0x90
-
+/* Цвета */
+//TODO: Больше цветов
 #define COLOR_YELLOW 		0xFFE0
 #define COLOR_BLACK 		0x0000
 #define COLOR_WHITE 		0xFFFF
 #define COLOR_INIT 			COLOR_YELLOW
 
 #define DOT_WIDTH 4
-
-typedef struct xy
-{
-	u16 x;
-	u16 y;
-}xy_t;
+//Структура для координат нажатия на тачскрин 
+typedef struct xy {
+	uint16_t x;
+	uint16_t y;
+} xy_t;
 
 #define TOUCH_MAX_CACHE 8
+/* Функции работы с дисплеем */
+//Аппаратная перезагрузка дисплея
+void TFT_reset(void);
+//Инициализация дисплея
+void TFT_init(SPI_HandleTypeDef *_displaySPI);
+//Очистка дисплея (залитие белым)
+void TFT_clear(void);
+/* Функции работы с тачскрином */
+
+
+
+
+
+//Прочее говно
+void post_data(uint16_t data);
+void post_cmd(uint16_t index, uint16_t cmd);
 /**/
-void init_lcd_spi(void);
-void init_touch_spi(void);
-void post_data(u16 data);
-void post_cmd(u16 index, u16 cmd);
-/**/
-u16 get_touch_data(u16 cmd);
+uint16_t get_touch_data(uint16_t cmd);
 xy_t get_touch_xy(void);
-u8 get_point_xy(void);
-u8 draw_lcd(void);
+uint8_t get_point_xy(void);
+uint8_t draw_lcd(void);
 void init_lcd(void);
 
 void LCD_test(void);
+
 #endif
